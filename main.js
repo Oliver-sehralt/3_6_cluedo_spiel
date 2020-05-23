9ccc3a609e7e8ea379589cfa5cd5c241ae8dc0a8
Oliver-sehralt <schlueter1895@gmail.com>
2020-05-23 12:41:59 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,89 @@
+const suspectsArray = [
+    {
+        firstName: "Jacob",
+        lastName: "Green",
+        occupation: "Entrepreneur",
+        age: 45,
+        description: "He has a lot of connections"
+    },
+    {
+        firstName: "Doctor",
+        lastName: "Orchid",
+        ocupation: "Scientist",
+        age: 26,
+        description: "Phd in plant toxicology. Adopted daughter of Mr. Boddy"
+    },
+    {
+        firstName: "Victor",
+        lastName: "Plum",
+        occupation: "Designer",
+        age: 22,
+        description: "Billionaire video game designer"
+    },
+
+    {
+        firstName: "Kasandra",
+        lastName: "Scarlett",
+        occupation: "Actor",
+        age: 31,
+        description: "She is an A-list movie star with a dark past"
+    },
+
+    {
+        firstName: "Elenor",
+        lastName: "Peacock",
+        occupation: "Socialité",
+        age: 36,
+        description: "She is from a wealthy family and uses her status and money to earn popularity"
+    },
+
+    {
+        fistName: "Jack",
+        lastName: "Mustard",
+        occupation: "Retired Football player",
+        age: 62,
+        description: "He is a former football player who tries to get by on his former glory"
+    }
+];
+
+const weaponsArray = [
+    {
+        name: "rope",
+        weigth: 10
+    },
+    {
+        name: "knife",
+        weigth: 8
+    },
+    {
+        name: "candlestick",
+        weigth: 2
+    },
+    {
+        name: "dumbbell",
+        weigth: 30
+    },
+    {
+        name: "poison",
+        weigth: 2
+    },
+    {
+        name: "axe",
+        weigth: 15
+    },
+    {
+        name: "bat",
+        weigth: 13
+    },
+    {
+        name: "trophy",
+        weigth: 25
+    },
+    {
+        name: "pistol",
+        weigth: 20
+    },
+];
+
+const roomsArray = ["dinning room", "conservatory", "kitchen", "study", "library", "billiard room", "lounge", "ballroom",
+"hall", "spa", "living room", "observatory", "theater", "guest house", "patio"];
